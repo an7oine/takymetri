f72338e0f18ef7eaa9f4e31c18672c6ce9813772
Antti Hautaniemi <antti.hautaniemi@me.com>
2015-02-18 13:11:32 +0200

    Päivitetty taky-siirto.bat

--- taky-siirto.bat
+++ taky-siirto.bat
@@ -1,12 +1,8 @@
-if "%1"=="" goto ei_param
+@echo off
 
-X:
-cd \xpower\Local\takymetri
-copy %1 .
-echo "%1" | c:\code\sed "s#[^\\]*\\##g" | starttakym.bat
-if errorlevel 1 pause
-exit
-
-:ei_param
-echo (ajetaan: TAKY-SIIRTO c:\...\tiedosto.xyz)
-pause
+if NOT "%~1"=="" (
+	cd /D X:\xpower\Local\takymetri
+	copy %1 .
+	echo "%~nx1" | starttakym.bat
+	if errorlevel 1 pause
+)
